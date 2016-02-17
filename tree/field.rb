3d53571511a70227235610dc7663643db6c06100
Antoine Joulie <antoine.joulie@rightscale.com>
2016-02-17 14:54:36 -0800

    Type is 1 of string,number,boolean,array,composite
      If field type is not one of those, then we assume it is a custom media-type and fallback
    to composite type

--- tree/field.rb
+++ tree/field.rb
@@ -20,6 +20,13 @@ module Tree
     end
 
     class Type < Tree::Base
+      FIELD_TYPE = %w(string number boolean array composite)
+
+      def initialize(value)
+        FIELD_TYPE.include?(value) or value = "composite"
+        super(value)
+      end
+
       def to_s(tab = 0)
         "#{' '*tab*2}type \"#{@value}\""
       end
