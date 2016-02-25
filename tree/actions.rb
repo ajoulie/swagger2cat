a8a999a33cb6ae2659ddd4a007ed3020daf621f4
Antoine Joulie <antoine.joulie@rightscale.com>
2016-02-24 23:18:53 -0800

    Enforce action to have a real name for now
      For now bame must match ^[a-zA-Z0-9]+$. This remove {path:*} for kubernetes actions
    Let's see later what we want to do with {path:*}.

--- tree/actions.rb
+++ tree/actions.rb
@@ -2,6 +2,8 @@ module Tree
   class Actions < Tree::Base
     include Tree::Sequence
 
+    ACTION_REGEXP = /^[a-zA-Z0-9]+$/
+
     def initialize(apis, resource_name)
       super("")
       @resource_name= resource_name
@@ -22,8 +24,12 @@ module Tree
 
     def add_actions
       @apis.each do |api|
+        # TODO be more generic
         if api["path"].match(/\/#{@resource_name}\/{name}\/([^\/]*)$/)
-          add Tree::Action.new(api["operations"].first, api["path"], $1)
+          action = $1
+          if action.match(ACTION_REGEXP)
+            add Tree::Action.new(api["operations"].first, api["path"], action)
+          end
         else
           api["operations"].each do |operation|
             add Tree::Action.new(operation, api["path"])
