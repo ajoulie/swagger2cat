90cd30c23c6fc904b7c54299031fa800dcbb9627
Antoine Joulie <antoine.joulie@rightscale.com>
2016-02-26 14:31:27 -0800

    Fix field.rb indentaton

--- swagger2_cat/node/field.rb
+++ swagger2_cat/node/field.rb
@@ -1,35 +1,35 @@
 module Swagger2Cat
-	module Node
-		class Field < Base
-			include Shape::Block
+  module Node
+    class Field < Base
+      include Shape::Block
 
-			def initialize(value, json)
-				super(value)
-				type(json.fetch("type") {json.fetch("$ref")})
-				required(true) if json["required"]
-			end
+      def initialize(value, json)
+        super(value)
+        type(json.fetch("type") {json.fetch("$ref")})
+        required(true) if json["required"]
+      end
 
-			private
-			def type(value)
-				add Type.new(value)
-			end
+      private
+      def type(value)
+        add Type.new(value)
+      end
 
-			def required(value)
-				add Required.new(value)
-			end
+      def required(value)
+        add Required.new(value)
+      end
 
-			class Type < Base
-				FIELD_TYPE = %w(string number boolean array composite)
+      class Type < Base
+        FIELD_TYPE = %w(string number boolean array composite)
 
-				def initialize(value)
-					FIELD_TYPE.include?(value) or value = "composite"
-					super(value)
-				end
+        def initialize(value)
+          FIELD_TYPE.include?(value) or value = "composite"
+          super(value)
+        end
 
-				def to_s(tab = 0)
-					"#{' '*tab*2}type \"#{@value}\""
-				end
-			end
-		end
-	end
+        def to_s(tab = 0)
+          "#{' '*tab*2}type \"#{@value}\""
+        end
+      end
+    end
+  end
 end
